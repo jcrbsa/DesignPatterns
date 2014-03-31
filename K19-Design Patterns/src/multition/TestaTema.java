d9c052252b9fdbd452dc8832295e5ef6e46c1e14
Richardson Bruno <richardsonbruno@gmail.com>
2014-03-30 22:22:41 -0300

    Multition

---
+++ K19-Design Patterns/src/multition/TestaTema.java
@@ -0,0 +1,14 @@
+package multition;
+
+public class TestaTema {
+	public static void main(String[] args) {
+		Tema temaFire = Tema.getInstance(Tema.SKY);
+		System.out.println(" Tema " + temaFire.getNome());
+		System.out.println(" Cor Da Fonte : " + temaFire.getCorDaFonte());
+		System.out.println(" Cor Do Fundo : " + temaFire.getCorDoFundo());
+		Tema temaFire2 = Tema.getInstance(Tema.FIRE);
+		System.out.println(" -------- ");
+		System.out.println(" Comparando as referências ... ");
+		System.out.println(temaFire == temaFire2);
+	}
+}
